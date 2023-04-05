44483e78fab594224ee59bedf4a68793fdc7b5bd
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-04-05 14:53:21 +0530

    Create greaterof3.java

---
+++ greaterof3.java
@@ -0,0 +1,23 @@
+package greater;
+
+public class Great {
+
+	public static void main(String[] args) {
+		int x=23,y=45,z=10;
+	
+			if(x>y)
+			{
+			  if(x>z)
+				  System.out.println("x is greatest");
+			  else
+				  System.out.println("z is greatest");
+			}
+			else if(y>z)
+				System.out.println("y is greatest");
+			else
+				System.out.println("z is greatest");
+	
+
+	}
+
+}
