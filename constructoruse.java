f73257433b5ba716c670a82726b878d797884805
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-04-12 14:14:29 +0530

    Create constructoruse.java

---
+++ constructoruse.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class SimpleObject {
+
+	SimpleObject()
+	{
+		System.out.println("I have been printed using constructor ");
+	}
+	public static void main(String[] args)
+	{
+		SimpleObject a = new SimpleObject();
+	}
+}
+	
