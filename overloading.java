4aaae54236f6c55c9b658f3b087e3b1e9939b9bb
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-04-19 14:48:59 +0530

    Update overloading.java

--- overloading.java
+++ overloading.java
@@ -13,10 +13,24 @@ public class overload
 		z=x-y;
 		System.out.println("add1"+z);
 	}
+	void add1(float x, float y,float z)
+	{
+		float f;
+		f=x+y+z;
+		System.out.println("add1"+f);
+	}
+	void add1(double x,double y)
+	{
+		double z;
+		z=x+y;
+		
+		System.out.println("add1"+z);
+	}	
 	public static void main(String[] args)
 	{
 		overload a = new overload();
-		a.add1(10,20);
-		a.add1(10.3f,10.0f);
+		a.add1(10,20); //int 
+		a.add1(10.3f,10.0f); //float
+		a.add1(10.2,10.2); //double 
 }
 }
