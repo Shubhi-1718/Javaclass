f7e9419c8011b324e2dbbbaed79255f3d8569aaf
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-05-03 14:19:06 +0530

    Update shallow clone.java

--- shallow clone.java
+++ shallow clone.java
@@ -1,36 +1,32 @@
-class Deepclone
+class Test 
 {
 	int x,y;
+	
 }
-class DeepClone2 implements Cloneable
+class Test2 implements Cloneable
 {
 	int a,b;
-	Deepclone c= new Deepclone();
+	Test c= new Test();
 	public Object clone() throws CloneNotSupportedException
 	{
-		DeepClone2 t2 = new DeepClone2();
-		t2.c = new Deepclone();
-		t2.c.x=c.x;
-		t2.c.y=c.y;
-		return (DeepClone2)super.clone();
+		return super.clone();
 	}
 }
-public class Cloning
-{
+public class TEST3 {
 
 	public static void main(String[] args) throws CloneNotSupportedException
 	{
-		DeepClone2 t =new DeepClone2();// TODO Auto-generated method stub
+		Test2 t =new Test2();// TODO Auto-generated method stub
 		t.a=10;
 		t.b=20;
 		t.c.x=30;
 		t.c.y=40;
-		DeepClone2 t3=(DeepClone2)t.clone();
-		t3.a=100;
-		t3.c.x=300;
+		Test2 t2=(Test2)t.clone();
+		t2.a=100;
+		t2.c.x=300;
+		t2.c.y=700;
 		System.out.println(t.a+" "+t.b+" "+t.c.x+" "+t.c.y);
-		System.out.println(t3.a+" "+t3.b+" "+t3.c.x+" "+t3.c.y);
-
+		System.out.println(t2.a+" "+t2.b+" "+t2.c.x+" "+t2.c.y);
 	}
 
 }
