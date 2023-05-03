6dd6e7efc320ae64b05778bf49d4805bbd1b10b5
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-05-03 14:39:20 +0530

    Create Deep Cloning.java

---
+++ Deep Cloning.java
@@ -0,0 +1,36 @@
+class Deepclone
+{
+	int x,y;
+}
+class DeepClone2 implements Cloneable
+{
+	int a,b;
+	Deepclone c= new Deepclone();
+	public Object clone() throws CloneNotSupportedException
+	{
+		DeepClone2 t = (DeepClone2)super.clone();
+		t.c = new Deepclone();
+		t.c.x=c.x;
+		t.c.y=c.y;
+		return t;
+	}
+}
+public class Cloning
+{
+
+	public static void main(String[] args) throws CloneNotSupportedException
+	{
+		DeepClone2 t =new DeepClone2();// TODO Auto-generated method stub
+		t.a=10;
+		t.b=20;
+		t.c.x=30;
+		t.c.y=40;
+		DeepClone2 t3=(DeepClone2)t.clone();
+		t3.a=100;
+		t3.c.x=300;
+		System.out.println(t.a+" "+t.b+" "+t.c.x+" "+t.c.y);
+		System.out.println(t3.a+" "+t3.b+" "+t3.c.x+" "+t3.c.y);
+
+	}
+
+}
