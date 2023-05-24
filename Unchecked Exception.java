e05c0c2e9574973223d0121edb01858b81bc6206
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-05-24 14:12:52 +0530

    Create Unchecked Exception.java

---
+++ Unchecked Exception.java
@@ -0,0 +1,20 @@
+class Userdefined 
+{
+	public static void method(int age)
+	{
+		if(age<18)
+			throw new ArithmeticException("Person not eligible to vote");
+		else
+			System.out.println("Person eligible to vote");
+		
+	}
+}
+public class Userclass {
+
+	public static void main(String[] args) 
+	{
+		Userdefined.method(2);
+		System.out.println("Use of throw");
+	}
+
+}
