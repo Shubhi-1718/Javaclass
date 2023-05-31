4fbd37b3475618a85169527daaa625eda2f16db3
Shubhangini Agrawal <119943709+Shubhi-1718@users.noreply.github.com>
2023-05-31 14:45:58 +0530

    Create synchronized block.java

---
+++ synchronized block.java
@@ -0,0 +1,38 @@
+class Share extends Thread
+{
+	static String msg[]= {"This","is","a","synchronized","variable"};
+	Share(String threadname)
+	{
+		super(threadname);
+	}
+	public void run() 
+	{
+	display(getName());		
+	}
+	public void display(String threadN)
+	{
+		synchronized(this)
+		{
+		for(int i=0;i<=4;i++)
+	System.out.println(threadN+msg[i]);
+		
+		try
+		{
+			this.sleep(1000);
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	}
+}
+public class SynThread {
+public static void main(String args[])
+{
+	Share t1=new Share("Thread1 ");
+	t1.start();
+	Share t2=new Share("Thread2 ");
+	t2.start();
+}
+}
